e10cce6000e7fec1022a03898be6471444dde67a
vidhidutta <vidhidutta@gmail.com>
2025-07-27 09:57:48 +0100

    Update __init__.py

---
+++ ankigenerator/__init__.py
@@ -0,0 +1,6 @@
+"""
+Package initializer for `ankigenerator`.
+
+This file marks the `ankigenerator` directory as a Python package so that modules
+contained within can be imported using the standard dotted module syntax.
+"""
