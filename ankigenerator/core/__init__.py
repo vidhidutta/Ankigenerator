f500013ad503bcc93d2ef3d04ff6ecbece9be651
vidhidutta <vidhidutta@gmail.com>
2025-07-27 09:58:15 +0100

    Update __init__.py

---
+++ ankigenerator/core/__init__.py
@@ -0,0 +1,7 @@
+"""
+Package initializer for `ankigenerator.core`.
+
+This file marks the `core` subpackage as a Python package so that modules
+contained within (e.g., `image_occlusion.py`) can be imported using
+`ankigenerator.core`.
+"""
